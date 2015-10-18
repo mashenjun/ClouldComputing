474607827689d50de1488b6d64ddd122573f8b53
Yun Lu <luyun1991sea@gmail.com>
2015-10-18 22:29:50 +0200

    listen, fetch data, return

--- MyProject/Header/listening_thread.py
+++ MyProject/Header/listening_thread.py
@@ -1,44 +1,71 @@
-__author__ = 'mashenjun'
+__author__ = 'yunlu'
 
 import sys,os.path
 import threading
 import time
-
+#import static
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-from SQS import handler as SQSHander
-import Static.static_create as create_static
+from SQS import handler as SQSHandler
+#import Static.static_create as create_static
+#import Static.static_handler as Shandler
 
-create_static.create_static_data()
+#create_static()
 
-sqs,client = SQSHander.connect_sqs()
-queue=SQSHander.get_queue(sqs,'TestSQS')
+sqs = SQSHandler.connect_to_sqs()
+client= SQSHandler.get_client()
+queue=SQSHandler.get_queue(sqs,'TestSQS')
 
 exitFlag = 0
+dict={'a': 3, 'b': 2}
 
-def listening(threadName, delay, list_idle, list_busy):
+def listening(threadName, delay):
     while True:
         if exitFlag:
             threadName.exit()
         time.sleep(delay)
-        msg_list =SQSHander.receive_multi_msg(client,queue,10)
+        msg=SQSHandler.receive_multi_msg(client, queue, 1) 
+        if (len(msg)>1):
+            user=SQSHandler.response(msg)
+            print user
+            dict[user]=dict[user]-1
+            print dict
+            if (dict[user]==0):
+                print "user %s could fetch now " % user
+                user_t = fetch_data(wait_finish,user)
+        else:
+            print "No msg now"
+            time.sleep(3*delay)
+#        SQSHandler.delete_msg(client, msg, queue)
+        
 
-
-class myThread (threading.Thread):
-    def __init__(self, threadID, name,list_idle,list_busy):
+class listen_thread(threading.Thread):
+    def __init__(self, threadID, name):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
-        self.list_idle = list_idle
-        self.list_busy = list_busy
-    def run(self):
-        listening(self.name, 1, self.list_idle,self.list_busy)
 
+    def run(self):
+        listening(self.name, 1)
+          
+class fetch_data(threading.Thread):
+    def __init__(self, t, *args):
+        threading.Thread.__init__(self, target=t, args=args)
+        self.start()
+
+def wait_finish(user):
+    time.sleep(5) 
+    print "user %s is finished now " % user         
 # Create new threads
-thread1 = myThread(1, "Thread-1", 1)
+thread1 = listen_thread(1, "Thread-1")
 
 # Start new Threads
 thread1.start()
 
 
 print "Exiting Main Thread"
+
+
+
+
+
