0718f285a1e47c20958953867f58982866df1021
Yun Lu <luyun1991sea@gmail.com>
2015-10-18 22:48:10 +0200

    sorry...Two queues...

--- MyProject/Worker/main.py
+++ MyProject/Worker/main.py
@@ -15,14 +15,16 @@ logger = custome_logger.get_logger(__name__)
 conn_s3 = S3_handler.connect_to_S3()
 conn_sqs = SQS_handler.connect_to_sqs()
 
-QUEUE_NAME = config.ConfigSectionMap()["sqs_input_queue"]
+
+INPUT_QUEUE_NAME = config.ConfigSectionMap()["sqs_input_queue"]
+OUTPUT_QUEUE_NAME = config.ConfigSectionMap()["sqs_output_queue"]
 #Create a msg in the TestSQS queue
 #create_msg(sqs,'TestSQS','AHAHAH')
 #receive the msg and print out the Body
 client= SQS_handler.get_client()
-queue=SQS_handler.get_queue(conn_sqs,QUEUE_NAME)
-logger.info(queue)
-msg=SQS_handler.receive_msg(client,queue)
+input_queue=SQS_handler.get_queue(conn_sqs,INPUT_QUEUE_NAME)
+logger.info(input_queue)
+msg=SQS_handler.receive_msg(client,input_queue)
 msg_content = SQS_handler.response(msg)
 message_list = msg_content.split("/")
 name = message_list[0]
@@ -33,5 +35,5 @@ imageProcess.process_image(filename)
 S3_handler.send_file(conn_s3,name)
 S3_handler.clear_local_folder()
 my_instanceId=EC2_handler.get_local_instanceId()
-SQS_handler.create_msg(conn_sqs, QUEUE_NAME,join(name,filename)+"@"+my_instanceId)
-SQS_handler.delete_msg(client,msg,queue)
+SQS_handler.create_msg(conn_sqs, OUTPUT_QUEUE_NAME,join(name,filename)+"@"+my_instanceId)
+SQS_handler.delete_msg(client,msg,input_queue)
